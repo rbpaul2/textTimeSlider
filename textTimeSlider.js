b4169f7aafea2c8f3d7dc1f5894aa9aca08cd931
Rohan Paul <rohanpaul@Rohans-MBP.attlocal.net>
2018-07-10 13:47:40 -0400

    Add optional loop parameter for synchronizing with other events

--- textTimeSlider.js
+++ textTimeSlider.js
@@ -1,21 +1,23 @@
 // text-time-slide
 jQuery.fn.extend({
-  textTimeSlider: function(text, time) {
+  textTimeSlider: function(text, time, loop = true) {
+    if (!text.length) return this;
   	//console.log(text, time);
   	var curr_text = text.shift();
   	var curr_time = time.shift();
-  	text.push(curr_text);
-  	time.push(curr_time);
-
-  	//Fade out 
-  	this.fadeOut(250, () => {
-  		this.html(curr_text); //Update text 
-  		this.fadeIn(250); //Fade in
-  	}); 
+    if (loop) {
+      text.push(curr_text);
+      time.push(curr_time);
+    }
   	
+  	// Fade out 
+  	this.fadeOut(250, () => {
+  		this.html(curr_text); // Update text 
+  		this.fadeIn(250); // Fade in
+  	}); 	
 
   	setTimeout(() => {
-  		this.textTimeSlide(text, time);
+  		this.textTimeSlider(text, time, loop);
   	}, curr_time);
 
   	return this;
